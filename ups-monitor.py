bdaf908bf77fbf8951069d9152596c46d2b7b7e1
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-10-05 10:10:56 +0300

    wip

--- ups-monitor.py
+++ ups-monitor.py
@@ -40,16 +40,43 @@ class Ups:
     def status(self):
         response = self.__command('Q1')
 
-        return response
+        response = response.split(' ')
+
+        return {
+            'input_voltage': response[0],
+            'input_fault_voltage': response[1],
+            'output_voltage': response[2],
+            'output_current_percentage': int(response[3]),
+            'input_frequency': response[4],
+            'battery_voltage': response[5],
+            'temperature': response[6],
+            'utility_fail': response[7][0],
+            'battery_low': response[7][1],
+            'bypass_active': response[7][2],
+            'ups_failed': response[7][3],
+
+            # If this is 0 then UPS is on battery and actively providing power
+            'ups_in_standby': response[7][4],
+
+            'test_in_progress': response[7][5],
+            'shutdown_active': response[7][6],
+            'beeper_on': response[7][7]
+        }
 
 
 def main():
     for (port, name) in UPS_TO_MONITOR:
         ups = Ups(port)
 
-        text = 'data={}'.format(ups.status())
+        status_json = ups.status()
+        status = []
 
-        print('power-statuses,{}'.format(text))
+        for key in status_json:
+            status.append(f'{key}={status_json[key]}')
+
+        status = ' '.join(status)
+
+        print(f'upses,name={name} {status}')
 
 
 if __name__ == "__main__":
